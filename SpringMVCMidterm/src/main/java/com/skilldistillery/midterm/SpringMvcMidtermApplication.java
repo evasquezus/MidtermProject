1d206da4f8ddc02a2abec70d45f473679b4b6feb
Edwin <giolambo@aol.com>
2019-03-07 13:55:39 -0700

    Stub out MVC/Boot project, link JPA project

---
+++ SpringMVCMidterm/src/main/java/com/skilldistillery/midterm/SpringMvcMidtermApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcMidtermApplication.class, args);
+	}
+
+}
