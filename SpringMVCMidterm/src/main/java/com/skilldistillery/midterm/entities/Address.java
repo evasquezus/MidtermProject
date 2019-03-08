6c3a0b1d10dda4c58e6c06e29b0202507c181396
Edwin <giolambo@aol.com>
2019-03-08 11:50:16 -0700

    Address entity added

---
+++ SpringMVCMidterm/src/main/java/com/skilldistillery/midterm/entities/Address.java
@@ -0,0 +1,103 @@
+package com.skilldistillery.midterm.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "apartment")
+	private String apartment;
+	
+	@Column(name = "city")
+	private String city;
+	
+	@Column(name = "state")
+	private String state;
+	
+	@Column(name = "state_abbreviation")
+	private String stateAbbreviation;
+	
+	@Column(name = "zip_code")
+	private int zipcode;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getApartment() {
+		return apartment;
+	}
+
+	public void setApartment(String apartment) {
+		this.apartment = apartment;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getStateAbbreviation() {
+		return stateAbbreviation;
+	}
+
+	public void setStateAbbreviation(String stateAbbreviation) {
+		this.stateAbbreviation = stateAbbreviation;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", address=" + address + ", apartment=" + apartment + ", city=" + city + ", state="
+				+ state + ", stateAbbreviation=" + stateAbbreviation + ", zipcode=" + zipcode + "]";
+	}
+
+	public Address() {
+		super();
+	}
+	
+	
+	
+}
