7f32cfa728ff3c7329066555de63523c9ce43277
Edwin <giolambo@aol.com>
2019-03-09 10:47:40 -0700

    Removed extra jsp files working on register.jsp

--- JPAMidterm/src/main/java/com/skillsdistillery/midterm/entities/User.java
+++ JPAMidterm/src/main/java/com/skillsdistillery/midterm/entities/User.java
@@ -17,6 +17,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class User {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
--- SpringMVCMidterm/src/main/java/com/skilldistillery/midterm/controller/MidTermController.java
+++ SpringMVCMidterm/src/main/java/com/skilldistillery/midterm/controller/MidTermController.java
@@ -4,20 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.midterm.data.MidtermMockDAO;
+import com.skillsdistillery.midterm.entities.Event;
+import com.skillsdistillery.midterm.entities.User;
 
 @Controller
 public class MidTermController {
 
-	
-//	@Autowired
-//	private MidtermMockDAO mockDao;
-	
-	@RequestMapping(path= {"/", "home.do"}, method = RequestMethod.GET)
+	@Autowired
+	private MidtermMockDAO mockDao;
+
+	@RequestMapping(path = { "/", "home.do" }, method = RequestMethod.GET)
 	public String index() {
 		return "WEB-INF/index.jsp";
 	}
-	
-	
+
+	@RequestMapping(path = "registerUser.do", params = { "registerUser" }, method = RequestMethod.POST)
+	public ModelAndView registerUser(User user) {
+		ModelAndView mv = new ModelAndView();
+		mockDao.createUser(user);
+		mv.addObject("userID", user);
+		mv.setViewName("WEB-INF/registerUser.jsp");
+		return mv;
+
+	}
+
 }
